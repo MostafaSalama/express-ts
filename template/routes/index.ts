185c561a8308c5b8356164e34ce084fe4de74271
Mostafa Salama <mostafalearning2016@gmail.com>
2019-03-13 15:34:58 +0200

    add routes files templates

---
+++ template/routes/index.ts
@@ -0,0 +1,11 @@
+import * as express from 'express';
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	console.log(`user is ${req.user}`);
+	res.render('index', { title: 'Express' });
+
+});
+
+export default router ;
