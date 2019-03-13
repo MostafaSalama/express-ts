5c8937d054171d944e39dcf57f228c2eaaa536f1
Mostafa Salama <mostafalearning2016@gmail.com>
2019-03-13 16:10:32 +0200

    add create app and create dir functions

--- app/create.js
+++ app/create.js
@@ -1,6 +1,27 @@
-const chalk = require('chalk') ;
-function createApp(name){
-    console.log(chalk.green('creating the app ...'))
+const chalk = require('chalk');
+const path = require('path');
+const fs = require('fs');
+
+const templateDir = path.join(__dirname,'..','template') ;
+
+/**
+ * creates a new dir(folder) in the  cwd
+ * @param name {string} name of the dir(folder)
+ * @return {string} the path of the dir(folder)
+ */
+function createDir(name) {
+    console.log(chalk.green(`creating the ${name} folder`));
+    const dirPath = path.join(process.cwd(),name) ;
+    try {
+        fs.mkdirSync(dirPath)
+    }
+    catch (e) {
+        console.log(chalk.red(e.message)) ;
+    }
+    return dirPath ;
 }
 
-module.exports = createApp ;
+function createApp(name) {
+	console.log(chalk.green('creating the app ...'));
+}
+module.exports = createApp;
